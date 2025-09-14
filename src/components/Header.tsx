01752dd40c37e1831c712d5e93e552840e1ae207
surjendu104 <surjendu@setuschool.com>
2025-09-14 10:37:18 +0530

    feat : change the name to "Log Sloth"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import classes from './Header.module.css';
 const Header = (): JSX.Element => {
   return (
     <div className={classes.head}>
-      <div className={classes.name}>Log River</div>
+      <div className={classes.name}>Log Sloth</div>
       <div className={classes.right}>
         <a href="">Home</a>
         <a href="">Dashboard</a>
