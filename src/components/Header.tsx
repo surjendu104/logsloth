4b3dec16ed3e91b0f3776bec8d74b972b32f37fd
surjendu104 <palsurjendu104@gmail.com>
2025-09-19 19:56:50 +0530

    feat : add header links and a bit responsive design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
 import { type JSX } from 'react';
-import { LuGithub } from 'react-icons/lu';
 import classes from './Header.module.css';
+import { BsArrowUpRight } from 'react-icons/bs';
 
 const Header = (): JSX.Element => {
   return (
     <div className={classes.head}>
       <div className={classes.name}>Log Sloth</div>
       <div className={classes.right}>
-        <a href="">Home</a>
-        <a href="">Dashboard</a>
-        <a href="">
-          <LuGithub />
+        <a href="/">Home</a>
+        <a
+          href="https://github.com/surjendu104/logsloth"
+          className={classes.github}
+        >
+          <span>github</span> <BsArrowUpRight />
         </a>
       </div>
     </div>
