ef3f475d045f79d6783e20bcfba94de8759931bb
surjendu104 <palsurjendu104@gmail.com>
2025-09-20 14:47:11 +0530

    chore : some style changes in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,19 @@
 import { type JSX } from 'react';
 import classes from './Header.module.css';
 import { BsArrowUpRight } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const Header = (): JSX.Element => {
   return (
     <div className={classes.head}>
-      <div className={classes.name}>Log Sloth</div>
+      <Link className={classes.name} to={'/'}>Log Sloth</Link>
       <div className={classes.right}>
         <a href="/">Home</a>
         <a
           href="https://github.com/surjendu104/logsloth"
           className={classes.github}
         >
-          <span>github</span> <BsArrowUpRight />
+          <span>Github</span> <BsArrowUpRight />
         </a>
       </div>
     </div>
